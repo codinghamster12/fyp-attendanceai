aa5d167e873fc54fc1c02648d40e2bec069bc6e4
bisma akram <bisma.akram97@hotmail.com>
2020-12-22 16:48:15 +0500

    changes

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -2,6 +2,8 @@ import { HashRouter, Route } from "react-router-dom";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import Home from "./containers/Home";
+import AddStudent from "./components/Students/Add-Student";
+
 import "./App.css";
 import "./index.css";
 
@@ -11,6 +13,8 @@ function App() {
       <Route exact path="/" component={Home}></Route>
       <Route exact path="/login" component={Login}></Route>
       <Route exact path="/register" component={Signup}></Route>
+      <Route exact path="/add-student" component={AddStudent}></Route>
+
 
     </HashRouter>
   );
